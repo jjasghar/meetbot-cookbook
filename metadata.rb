631e371cbd90fb4cdb1c0ed2f54d9fede01b3926
JJ Asghar <jj@getchef.com>
2014-08-27 18:17:12 -0500

    1.0.0 release

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
 name             'meetbot'
 maintainer       'JJ Asghar'
 maintainer_email 'jj@getchef.com'
-license          ''
-description      'Installs/Configures meetbot-cookbook'
-long_description 'Installs/Configures meetbot-cookbook'
-version          '0.1.0'
+license          'Apache 2.0'
+description      'Installs/Configures meetbot'
+long_description 'Installs/Configures meetbot'
+version          '1.0.1'
 
 depends 'python'
