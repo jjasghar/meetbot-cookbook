a4c9a247ae4b54b20bd559af197e99e7fe4f104b
JJ Asghar <jj@getchef.com>
2014-08-29 14:00:45 -0500

    Added supports line to the metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,9 @@ maintainer_email 'jj@getchef.com'
 license          'Apache 2.0'
 description      'Installs/Configures meetbot'
 long_description 'Installs/Configures meetbot'
-version          '1.5.0'
+version          '1.5.1'
+
+supports 'ubuntu' , ">= 14.04"
+supports 'centos', ">= 6.5"
 
 depends 'python'
