3fbe7c246d8f42d7daa195e5cdb89b9403e1d825
JJ Asghar <jjasghar@gmail.com>
2014-08-29 12:45:17 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'jj@getchef.com'
 license          'Apache 2.0'
 description      'Installs/Configures meetbot'
 long_description 'Installs/Configures meetbot'
-version          '1.0.1'
+version          '1.5.0'
 
 depends 'python'
